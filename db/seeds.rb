4fd1f2d6455ab34b5dc6a1f24d5f5ae42b61f116
Derrick Chou <treez217@gmail.com>
2016-02-28 17:44:12 -0500

    It works!!!!

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+5.times do |i|
+  Article.create(title: "Article ##{i}", body: "A product.")
+end
